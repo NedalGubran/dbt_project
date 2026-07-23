79c3ecb0dce1073da15117bc838d0d43ee6fc544
NedalGubran <nedalj29@gmail.com>
2026-07-23 12:46:02 +0200

    Add staging weather model

--- models/staging/staging_weather_hourly.sql
+++ models/staging/staging_weather_hourly.sql
@@ -4,7 +4,6 @@ WITH hourly_raw AS (
         airport_code,
         station_id,
         JSON_ARRAY_ELEMENTS(extracted_data -> 'data') AS json_data
-
     FROM {{ source('weather_data', 'weather_hourly_raw') }}
 
 ),
@@ -16,12 +15,13 @@ hourly_data AS (
         station_id,
 
         (json_data->>'time')::TIMESTAMP AS timestamp,
+
         (json_data->>'temp')::NUMERIC AS temp_c,
         (json_data->>'dwpt')::NUMERIC AS dewpoint_c,
-        (json_data->>'rhum')::NUMERIC AS humidity_perc,
+        (json_data->>'rhum')::INTEGER AS humidity_perc,
         (json_data->>'prcp')::NUMERIC AS precipitation_mm,
         (json_data->>'snow')::INTEGER AS snow_mm,
-        ((json_data->>'wdir')::NUMERIC)::INTEGER AS wind_direction,
+        (json_data->>'wdir')::INTEGER AS wind_direction,
         (json_data->>'wspd')::NUMERIC AS wind_speed_kmh,
         (json_data->>'wpgt')::NUMERIC AS wind_peakgust_kmh,
         (json_data->>'pres')::NUMERIC AS pressure_hpa,
@@ -33,4 +33,4 @@ hourly_data AS (
 )
 
 SELECT *
-FROM hourly_data;
+FROM hourly_data
