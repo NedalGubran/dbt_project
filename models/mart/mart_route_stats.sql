16f396c1a554ad57194606b168b466c44a8017c4
NedalGubran <nedalj29@gmail.com>
2026-07-27 10:59:24 +0200

    Create mart

--- models/mart/mart_route_stats.sql
+++ models/mart/mart_route_stats.sql
@@ -1,30 +1,86 @@
-{{ config(materialized='view') }}
+{{ config(materialized='table') }}
+
+with routes as (
+
+    select
+
+        origin,
+
+        dest,
+
+        count(*) as total_flights,
+
+        count(distinct tail_number) as unique_airplanes,
+
+        count(distinct airline) as unique_airlines,
+
+        avg(actual_elapsed_time) as avg_actual_elapsed_time,
+
+        avg(arr_delay) as avg_arrival_delay,
+
+        max(arr_delay) as max_delay,
+
+        min(arr_delay) as min_delay,
+
+        sum(cancelled) as total_cancelled,
+
+        sum(diverted) as total_diverted
+
+    from {{ ref('prep_flights') }}
+
+    group by
+        origin,
+        dest
+
+)
 
 select
 
-origin,
-dest,
+    r.origin,
 
-count(*) as total_flights,
+    r.dest,
 
-count(distinct tail_number) as unique_airplanes,
+    r.total_flights,
 
-count(distinct airline) as unique_airlines,
+    r.unique_airplanes,
 
-avg(actual_elapsed_time) as avg_elapsed,
+    r.unique_airlines,
 
-avg(arr_delay) as avg_arrival_delay,
+    r.avg_actual_elapsed_time,
 
-max(arr_delay) as max_delay,
+    r.avg_arrival_delay,
 
-min(arr_delay) as min_delay,
+    r.max_delay,
 
-sum(cancelled) as cancelled,
+    r.min_delay,
 
-sum(diverted) as diverted
+    r.total_cancelled,
 
-from {{ ref('prep_flights') }}
+    r.total_diverted,
 
-group by
-origin,
-dest
+
+    origin_airport.name as origin_name,
+
+    origin_airport.city as origin_city,
+
+    origin_airport.country as origin_country,
+
+
+    destination_airport.name as destination_name,
+
+    destination_airport.city as destination_city,
+
+    destination_airport.country as destination_country
+
+
+from routes r
+
+
+left join {{ ref('prep_airports') }} origin_airport
+
+on origin_airport.faa = r.origin
+
+
+left join {{ ref('prep_airports') }} destination_airport
+
+on destination_airport.faa = r.dest
