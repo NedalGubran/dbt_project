c0e134de0f1a2183abcb86a5303370515efd3594
NedalGubran <nedalj29@gmail.com>
2026-07-27 10:37:25 +0200

    Create mart

--- models/mart/mart_faa_stats.sql
+++ models/mart/mart_faa_stats.sql
@@ -1,16 +1,69 @@
-SELECT
+{{ config(materialized='table') }}
 
-origin,
+with departures as (
 
-COUNT(*) AS total_flights,
+    select
+        origin as airport_code,
+        count(distinct dest) as departure_connections,
+        count(*) as departures_total,
+        sum(cancelled) as cancelled_departures,
+        sum(diverted) as diverted_departures,
+        sum(case when cancelled = 0 then 1 else 0 end) as actual_departures,
+        count(distinct tail_number) as unique_airplanes,
+        count(distinct airline) as unique_airlines
 
-SUM(
-CASE
-WHEN cancelled = 1 THEN 1
-ELSE 0
-END
-) AS cancelled
+    from {{ ref('prep_flights') }}
 
-FROM {{ ref('prep_flights') }}
+    group by origin
 
-GROUP BY origin
+),
+
+arrivals as (
+
+    select
+        dest as airport_code,
+        count(distinct origin) as arrival_connections,
+        count(*) as arrivals_total,
+        sum(cancelled) as cancelled_arrivals,
+        sum(diverted) as diverted_arrivals,
+        sum(case when cancelled = 0 then 1 else 0 end) as actual_arrivals
+
+    from {{ ref('prep_flights') }}
+
+    group by dest
+
+)
+
+select
+
+    coalesce(d.airport_code, a.airport_code) as airport_code,
+
+    coalesce(departure_connections,0) as departure_connections,
+    coalesce(arrival_connections,0) as arrival_connections,
+
+    coalesce(departures_total,0)
+    + coalesce(arrivals_total,0) as total_flights,
+
+    coalesce(cancelled_departures,0)
+    + coalesce(cancelled_arrivals,0) as total_cancelled,
+
+    coalesce(diverted_departures,0)
+    + coalesce(diverted_arrivals,0) as total_diverted,
+
+    coalesce(actual_departures,0)
+    + coalesce(actual_arrivals,0) as actual_flights,
+
+    unique_airplanes,
+    unique_airlines,
+
+    a.name as airport_name,
+    a.city,
+    a.country
+
+from departures d
+
+full join arrivals ar
+on d.airport_code = ar.airport_code
+
+left join {{ ref('prep_airports') }} a
+on a.faa = coalesce(d.airport_code, ar.airport_code)
