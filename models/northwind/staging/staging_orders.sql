0376590a5826d18ed9b86112bbc2704120cfca48
NedalGubran <nedalj29@gmail.com>
2026-07-28 14:34:12 +0200

    Creatmini

--- models/northwind/staging/staging_orders.sql
+++ models/northwind/staging/staging_orders.sql
@@ -11,7 +11,7 @@ SELECT
     employee_id,
     order_date::DATE AS order_date,
     required_date::DATE AS required_date,
-    ship_date::DATE AS shipped_date,
+    shipped_date::DATE AS shipped_date,
     ship_via,
     ship_city,
     ship_country
